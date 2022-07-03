e804f9d0dc39a92c16b38b8757724c3ea240215b
Gabriel Roberto <gabrielrobertoal@gmail.com>
2022-07-03 08:58:28 -0300

    project creation

---
+++ src/App.styles.ts
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const Containder = styled.div``;
+
+export const Header = styled.header`
+  height: 150px;
+  width: 100%;
+  background: linear-gradient(to bottom, #000b3d, #00198a);
+  text-align: center;
+`;
+
+export const HeaderTitle = styled.h1`
+  margin: 0;
+  padding: 30px 0 0 0;
+  color: #eee;
+  font-weight: 100;
+`;
+
+export const Body = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 980px;
+  background-color: #fff;
+  margin: auto;
+  margin-bottom: 50px;
+`;
---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import * as C from './App.styles';
+
+function App() {
+  return (
+    <C.Containder>
+      <C.Header>
+        <C.HeaderTitle>Sistema Financeiro</C.HeaderTitle>
+      </C.Header>
+      <C.Body>
+        Content
+      </C.Body>
+    </C.Containder>
+  );
+}
+
+export default App;
